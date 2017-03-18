1b8e10c21dfcbcc00105597c37e6567d5a7687fd
RobzDoom <jorgecruz.rd@gmail.com>
2017-03-18 12:38:20 -0700

    finished combining reducers

--- learn-redux/client/reducers/index.js
+++ learn-redux/client/reducers/index.js
@@ -3,4 +3,13 @@
  */
 // We are going to combine all of our reducers in this page
 
-import { combineReducers } from 'react-redux'
+import { combineReducers } from 'redux'
+import { routerReact } from 'react-router-redux'
+
+import post from './post'
+import comment from './comment'
+
+const rootReducers = combineReducers({post, comment,
+    router: routerReducer});
+
+export default rootReducers;
